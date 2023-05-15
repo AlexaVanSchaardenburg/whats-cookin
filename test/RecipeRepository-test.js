37f8b0598bf04def89bd05f568893f9376bb0696
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-05-15 15:34:54 -0600

    Refactor to format test file

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -6,7 +6,6 @@ const {filterByTag} = require('../src/RecipeRepository');
 describe('Filter', () => {
   
   let recipes;
-
   beforeEach(() => {
   return recipes = mockRecipeData
   });
@@ -15,115 +14,116 @@ describe('Filter', () => {
     
     const filteredRecipes1 = filterByTag(recipes,'sauce')
 
-    expect(filteredRecipes1).to.deep.equal([{
-      "id": 412309,
-      "image": "https://spoonacular.com/recipeImages/412309-556x370.jpeg",
-      "ingredients": [
-        {
-          "id": 1002030,
-          "quantity": {
+    expect(filteredRecipes1).to.deep.equal([
+      {
+        "id": 412309,
+        "image": "https://spoonacular.com/recipeImages/412309-556x370.jpeg",
+        "ingredients": [
+          {
+            "id": 1002030,
+            "quantity": {
             "amount": 4,
             "unit": "teaspoons"
           }
-        },
-        {
-          "id": 19334,
-          "quantity": {
-            "amount": 8,
-            "unit": "tablespoons"
+          },
+          {
+            "id": 19334,
+            "quantity": {
+              "amount": 8,
+              "unit": "tablespoons"
+            }
+          },
+          {
+            "id": 1001,
+            "quantity": {
+              "amount": 2,
+              "unit": "cups"
+            }
+          },
+          {
+            "id": 4582,
+            "quantity": {
+              "amount": 4,
+              "unit": "servings"
+            }
+          },
+          {
+            "id": 2031,
+            "quantity": {
+              "amount": 4,
+              "unit": "teaspoons"
+            }
+          },
+          {
+            "id": 5100,
+            "quantity": {
+              "amount": 1,
+              "unit": "pound"
+            }
+          },
+          {
+            "id": 2009,
+            "quantity": {
+              "amount": 4,
+              "unit": "teaspoons"
+            }
+          },
+          {
+            "id": 1022020,
+            "quantity": {
+              "amount": 4,
+              "unit": "teaspoons"
+            }
+          },
+          {
+            "id": 6168,
+            "quantity": {
+              "amount": 8,
+              "unit": "cups"
+            }
+          },
+          {
+            "id": 9176,
+            "quantity": {
+              "amount": 0.5,
+              "unit": "cup"
+            }
+          },
+          {
+            "id": 2026,
+            "quantity": {
+              "amount": 4,
+              "unit": "teaspoons"
+            }
+          },
+          {
+            "id": 1042047,
+            "quantity": {
+              "amount": 1.5,
+              "unit": "tablespoons"
+            }
+          },
+          {
+            "id": 1042047,
+            "quantity": {
+              "amount": 4,
+              "unit": "teaspoons"
+              }
           }
-        },
-        {
-          "id": 1001,
-          "quantity": {
-            "amount": 2,
-            "unit": "cups"
-          }
-        },
-        {
-          "id": 4582,
-          "quantity": {
-            "amount": 4,
-            "unit": "servings"
-          }
-        },
-        {
-          "id": 2031,
-          "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-          }
-        },
-        {
-          "id": 5100,
-          "quantity": {
-            "amount": 1,
-            "unit": "pound"
-          }
-        },
-        {
-          "id": 2009,
-          "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-          }
-        },
-        {
-          "id": 1022020,
-          "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-          }
-        },
-        {
-          "id": 6168,
-          "quantity": {
-            "amount": 8,
-            "unit": "cups"
-          }
-        },
-        {
-          "id": 9176,
-          "quantity": {
-            "amount": 0.5,
-            "unit": "cup"
-          }
-        },
-        {
-          "id": 2026,
-          "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-          }
-        },
-        {
-          "id": 1042047,
-          "quantity": {
-            "amount": 1.5,
-            "unit": "tablespoons"
-          }
-        },
-        {
-          "id": 1042047,
-          "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-          }
-        }
-      ],
-      "instructions": [
-        {
+       ],
+       "instructions": [
+         {
           "instruction": "Mix the hot sauce, butter, mango habanero sauce, brown sugar, chili powder, garlic powder, onion powder, black pepper, cayenne pepper and seasoning salt in a bowl. Stir vigorously until completely combined.",
           "number": 1
-        }
-      ],
-      "name": "Dirty Steve's Original Wing Sauce",
-      "tags": [
-        "sauce"
-      ]
-    }])
+         }
+       ],
+       "name": "Dirty Steve's Original Wing Sauce",
+       "tags": [
+          "sauce"
+        ]
+      }])
 
-  });
+    });
   it('Should return a message if no recieps match the tag filter', () => {
 
     const filteredRecipes2 = filterByTag(recipes,'javascript')
@@ -131,34 +131,131 @@ describe('Filter', () => {
     expect(filteredRecipes2).to.equal("Sorry, No Recipes Were Found!")
   })
   it('Should filter recipes by search', function(){
-      const filteredRecipes = filterByName('')
+      const filteredRecipes = filterByName('cookie')
 
-      expect(filteredRecipes).to.deep.equal(/* Array of sample data filterd for key word appears here */)
+      expect(filteredRecipes).to.deep.equal([
+        {
+          "id": 595736,
+          "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+          "ingredients": [
+            {
+              "id": 20081,
+              "quantity": {
+                "amount": 1.5,
+                "unit": "c"
+              }
+            },
+            {
+              "id": 18372,
+              "quantity": {
+                "amount": 0.5,
+                "unit": "tsp"
+              }
+            },
+            {
+              "id": 1123,
+              "quantity": {
+                "amount": 1,
+                "unit": "large"
+              }
+            },
+            {
+              "id": 19335,
+              "quantity": {
+                "amount": 0.5,
+                "unit": "c"
+              }
+            },
+            {
+              "id": 19206,
+              "quantity": {
+                "amount": 3,
+                "unit": "Tbsp"
+              }
+            },
+            {
+              "id": 19334,
+              "quantity": {
+                "amount": 0.5,
+                "unit": "c"
+              }
+            },
+            {
+              "id": 2047,
+              "quantity": {
+                "amount": 0.5,
+                "unit": "tsp"
+              }
+            },
+            {
+              "id": 1012047,
+              "quantity": {
+                "amount": 24,
+                "unit": "servings"
+              }
+            },
+            {
+              "id": 10019903,
+              "quantity": {
+                "amount": 2,
+                "unit": "c"
+              }
+            },
+            {
+              "id": 1145,
+              "quantity": {
+                "amount": 0.5,
+                "unit": "c"
+              }
+            },
+            {
+              "id": 2050,
+              "quantity": {
+                "amount": 0.5,
+                "unit": "tsp"
+              }
+            }
+          ],
+          "instructions": [
+            {
+              "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+              "number": 1
+            },
+            {
+              "instruction": "Add egg and vanilla and mix until combined.",
+              "number": 2
+            },
+            {
+              "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
+              "number": 3
+            },
+            {
+              "instruction": "Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.",
+              "number": 4
+            },
+            {
+              "instruction": "Bake for 9 to 10 minutes, or until you see the edges start to brown.",
+              "number": 5
+            },
+            {
+              "instruction": "Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.",
+              "number": 6
+            }
+          ],
+          "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+          "tags": [
+            "antipasti",
+            "starter",
+            "snack",
+            "appetizer",
+            "antipasto",
+            "hor d'oeuvre"
+          ]
+        }])
   });
   it('Should return a message if no recipes match the search', function(){
-      const filteredRecipes = filterByName('')
+      const filteredRecipes = filterByName('gabblygookblahblahblah')
 
-      expect(filteredRecipes).to.equal(/* message */)
+      expect(filteredRecipes).to.equal("Sorry, No Recipes Were Found!")
   });
 });
-
-// describe('Filter by name', function(){
-//   const recipes = recipeSampleData
-//   it('should be a function', function(){
-//       filterByName = filterByName()
-//       expect(filterByName).to.be.a('function')
-//   });
-//   it('Should return an array of recipes with matching name', function(){
-//       const filteredRecipes = filterByName('')
-
-//       expect(filteredRecipes).to.deep.equal(/* Array of sample data filterd for key word appears here */)
-//   });
-//   it('Should return an array of recipes with matching name', function(){
-//       const filteredRecipes = filterByName('')
-
-//       expect(filteredRecipes).to.deep.equal(/* Array of sample data filterd for key word appears here */)
-//   });
-// });
-// describe('Filter by tag', function(){
-
-// });
