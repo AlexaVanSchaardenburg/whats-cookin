1af092ca0921aa17a140d0dea64d15bd291a8ea6
Race Osuna <raceosuna@gmail.com>
2023-05-14 20:01:09 -0600

    feat: filter recipe by tag function + TDD with happy/sad paths

--- src/RecipeRepository.js
+++ src/RecipeRepository.js
@@ -1,8 +1,11 @@
-const filterByTag = (tag) => {
-  console.log()
-  return data.filter((recipe) => recipe.tags.includes(tag))
-    .map(recipe => recipe.name)
-  }
+const filterByTag = (data, tag) => {
+  const filteredRecipes = data.filter((recipe) => recipe.tags.includes(tag))
+   if (filteredRecipes.length < 1) {
+    return "Sorry, No Recipes Were Found!"
+   } else {
+    return filteredRecipes
+   }
+}
 
 
 module.exports = {
