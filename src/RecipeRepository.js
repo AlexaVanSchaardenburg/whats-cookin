e04227204842ebd78cebf7336b36ed7be1ae1754
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-05-16 13:24:37 -0600

    feat: Pseudocode for calcRecipeCost function

--- src/RecipeRepository.js
+++ src/RecipeRepository.js
@@ -24,8 +24,26 @@ const filterByName = (data, search) => {
   }
 }
 
+const calcRecipeCost = () => {
+  //takes in recipe and outputs number
+  // needs to acess list of ingredients
+  //for each ingrident it needs to check if the ingrident is an ingredient in the ingredient list
+    //if yes access that ingredients cost and multiply it by the quantity needed from recipe and add that number to the counter
+  //then convert number into correct format and return as number with two decimal places
+
+  //reduce? forEach? 
+  /*
+  start with value 0
+  1. make list of ingredients
+  2. iterate through ingredients info array for each ingridient - return the ingreidents cost
+  3.  take cost and multiply it by quantity from recipe - return totalIngredientCost
+  4.  
+  */
+}
+
 
 module.exports = {
   filterByTag,
-  filterByName
+  filterByName,
+  calcRecipeCost
 };
