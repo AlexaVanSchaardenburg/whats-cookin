5f83258bd0474b366380c955a5cf82e41253dfc5
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-05-15 18:14:13 -0600

    feat: Build functionality to search by name

--- src/RecipeRepository.js
+++ src/RecipeRepository.js
@@ -7,7 +7,21 @@ const filterByTag = (data, tag) => {
    }
 }
 
+const filterByName = (data, search) => {
+  //want to take search and look in the datas names to check if that word is there 
+  //return that whole object if it is, if not return message that nothing was found
+  //use filter 
+  // searchLowercase = search.toLowerCase()
+  const filteredRecipes = data.filter((recipe) => recipe.name.includes(search.toLowerCase()))
+  if (filteredRecipes.length < 1) {
+    return "Sorry, No Recipes Were Found!"
+   } else {
+    return filteredRecipes
+   }
+}
+
 
 module.exports = {
   filterByTag,
+  filterByName
 };
