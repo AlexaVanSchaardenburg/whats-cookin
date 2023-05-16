fcf5a3131049534b26005033fbde6cd92e0002fa
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-05-15 19:04:55 -0600

    feat: Allow search by name to work regardless of case or word count

--- src/RecipeRepository.js
+++ src/RecipeRepository.js
@@ -11,22 +11,23 @@ const filterByName = (data, search) => {
   //want to take search and look in the datas names to check if that word is there 
   //return that whole object if it is, if not return message that nothing was found
   //use filter
-  // const filteredRecipes = data.filter((recipe) => {
-  //   console.log(recipe['name'.toLowerCase()])
-  //   console.log(search.toLowerCase())
-  //   return recipe['name'.toLowerCase()].includes(search.toLowerCase())
-  // })
-  const searchLowerCase = search.toLowerCase()
-  const searchProper  = searchLowerCase.charAt(0)
-  const filteredRecipes = data.filter((recipe) => {
-    console.log(recipe.name)
-    return recipe.name.includes(searchProper)
+  const searchAllLowerCase = search.toLowerCase()
+  console.log(searchAllLowerCase)
+  const arrOfWords = searchAllLowerCase.split(' ')
+  console.log(arrOfWords)
+  const searchProper = arrOfWords.map(word => {
+    return upperCaseWord = word.charAt(0).toUpperCase() + word.slice(1)}
+    )
+  console.log(searchProper)
+  const finalSearch = searchProper.join(' ')
+  const filteredRecipes = data.filter(recipe => {
+    return recipe.name.includes(finalSearch)
   })
-  // if (filteredRecipes.length < 1) {
-  //   return "Sorry, No Recipes Were Found!"
-  //  } else {
+  if(filteredRecipes.length < 1){
+    return "Sorry, No Recipes Were Found!"
+  }else{
     return filteredRecipes
-  //  }
+  }
 }
 
 
