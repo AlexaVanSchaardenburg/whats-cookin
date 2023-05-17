347980b9589419c097d6cc8099a5428c28a0ba54
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-05-17 17:30:59 -0600

    feat: Pseduocode for displaying a clicked on recipes info

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -4,24 +4,39 @@
 
 const hideDomElement = (element) => {
   element.classList.add('hidden')
-}
+};
 
 const showDomElement = (element) => {
   element.classList.remove('hidden')
-}
+};
 
 const showRecipesPage = (homePage, allRecipesPage) => {
   hideDomElement(homePage)
   showDomElement(allRecipesPage)
-}
+};
 
 const displayAllRecipes = (data, recipeBox) => {
-  console.log(data)
   data.forEach((recipe) => {
   recipeBox.innerHTML += `<article class="all-recipe-box" id="${recipe.id}">
   <img class="all-recipe-image" src="${recipe.image}">
   <h3>${recipe.name}</h3>
   </article>`})
-}
+};
 
-export {showRecipesPage, showDomElement, hideDomElement, displayAllRecipes}
+const showRecipePage = () => {
+  //needs to hide home page and all recipes page - FUNCTION
+};
+
+const selectRecipe = (e) => {
+    //iterate through the recipes data and find the recipe with the matching id to the elements id that was clicked. - FUNCTION
+    return selectedRecipe
+};
+
+const displayRecipe = (event) => {
+  //needs to take in an event target as the parameter -- event listener listens for click on page and returns the element that was clicked (child element) -- put event listener on allRecipesBox - IN SCRIPT.JS FILE
+  selectRecipe(event)
+  //display the selected recipes name, ingredients, instructions, and total cost on the individual recipe page using helper functions from RecipeRepository.js file
+  showRecipePage()
+};
+
+export {showRecipesPage, showDomElement, hideDomElement, displayAllRecipes, displayRecipe}
