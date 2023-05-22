4bd6cde83e760bc50589c5c7fa0f37a1319b57cd
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-05-22 17:07:22 -0600

    refactor: Remove comments and DRY up code in recipe repo

--- src/RecipeRepository.js
+++ src/RecipeRepository.js
@@ -1,28 +1,21 @@
-// const { usersData } = require("./data/users");
-// import {user} from './apiCalls'
-
-
-const filterByTag = (recipeData, tag) => {
-  const filteredRecipes = recipeData.filter((recipe) => recipe.tags.includes(tag.toLowerCase()))
-  
-  // Suggested Change: Put this conditional in a helper function; we use it multiple times
-  if (filteredRecipes.length) {
-    return filteredRecipes
+const findLength = (array) => {
+  if (array.length) {
+    return array
   } else {
     return "Sorry, No Recipes Were Found!"
   };
+};
+
+const filterByTag = (recipeData, tag) => {
+  const filteredRecipes = recipeData.filter((recipe) => recipe.tags.includes(tag.toLowerCase()))
+  return findLength(filteredRecipes)
 };
 
 const filterByName = (recipeData, searchInput) => {
   const searchTerms = searchInput.toLowerCase().split(' ');
   const formattedSearch = searchTerms.map(word => capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
   const filteredRecipes = recipeData.filter(recipe => recipe.name.includes(formattedSearch));
-  // See above suggestions
-  if (filteredRecipes.length) {
-    return filteredRecipes
-  } else {
-    return "Sorry, No Recipes Were Found!"
-  };
+  return findLength(filteredRecipes)
 };
 
 const getInstructions = (recipe) => {
@@ -38,37 +31,20 @@ const getIngredientNames = (ingredientData, recipe) =>  {
     const ingredientIndex = ingredientData.findIndex(ingredient => id === ingredient.id)
     return ingredientData[ingredientIndex].name
     });
-
   return ingredientNames;
 };
 
 const calcRecipeCost = (ingredientData, recipe) => {
-  /*
-  start with value 0
-  1. make list of ingredients ✅
-  2. map through recipe ingredients ✅
-    a. iterate through ingredients info array for each ingredient to find the ingredient info (returns object) ✅
-    b. take ingredientInfo that was found and get ingredient cost ✅
-    c. take cost and multiply it by quantity from recipe - return totalIngredientCost ✅
-  4.  add totalIngredientCost to counter ✅
-  5. return total cost ✅
-  6. transform totalcost into a number with two decimal places ✅
-  */
-
   const ingredientsCosts = recipe.ingredients.map(recipeIngredient => {
     const currentIngredient = ingredientData.find(ingredient => ingredient.id === recipeIngredient.id); 
     const currentIngredientCost = currentIngredient.estimatedCostInCents * recipeIngredient.quantity.amount
-
     return currentIngredientCost
   })
-
   const totalCost = parseFloat((ingredientsCosts.reduce((total, cost) => total + cost, 0)/100)).toFixed(2)
-
   return totalCost
 };
 
 const selectRandomUser = (users) => {
-  // console.log(users)
   const index = Math.floor(Math.random() * users.length - 1);
   const aUser = users[index];
   return aUser;
