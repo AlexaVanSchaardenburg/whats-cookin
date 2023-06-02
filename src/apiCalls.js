6dbf203af3a282189dc17d7bedcea8b80604ebc9
Race Osuna <raceosuna@gmail.com>
2023-06-02 07:52:29 -0600

    Feat: add error handling to api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,9 +5,29 @@ import { selectRandomUser } from '../src/RecipeRepository.js';
 import { currency } from './scripts.js'
 
 
-const usersResponse = fetch('http://localhost:3001/api/v1/users').then((response) => response.json()).catch(error => alert(`${error.message}: server down`));
-const recipeResponse = fetch('http://localhost:3001/api/v1/recipes').then(response => response.json()).catch(error => alert(`${error.message}: server down`));
-const ingredientsResponse = fetch('http://localhost:3001/api/v1/ingredients').then((response) => response.json()).catch(error => alert(`${error.message}: server down`));
+const usersResponse = fetch('http://localhost:3001/api/v1/users').then((response) => {
+  if(!response.ok) {
+    throw new Error(`${response.status}`)
+  } else {
+    return response.json();
+  }
+}).catch(error => alert(`${error.message}`));
+
+const recipeResponse = fetch('http://localhost:3001/api/v1/recipes').then(response => {
+  if(!response.ok) {
+    throw new Error(`${response.status}`)
+  } else {
+    return response.json();
+  }
+}).catch(error => alert(`${error.message}`));
+
+const ingredientsResponse = fetch('http://localhost:3001/api/v1/ingredients').then((response) => {
+  if(!response.ok) {
+    throw new Error(`${response.status}`)
+  } else {
+    return response.json();
+  }
+}).catch(error => alert(`${error.message}`));
 
 window.addEventListener('load', () => {
 Promise.all([usersResponse, recipeResponse, ingredientsResponse]).then(([users, recipes, ingredients]) => {
@@ -30,7 +50,7 @@ fetch(`https://api.frankfurter.app/currencies`)
       currency[0].innerHTML += `<option value="${entry[0]}">${entry[0]}</option>`;
     })
   })
-  .catch(error => alert(`${error.message}: server down`));
+  .catch(error => alert(`${error.message}`));
 
 export {
     recipeData,
